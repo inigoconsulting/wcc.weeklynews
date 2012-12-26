9f73da4c151d5a873a055e49f1c4216623622649
kulll <anwarbaik88@gmail.com>
2012-12-26 20:56:02 +0800

    wcc.weeklynews:     * added weeklynews.css     * can grab news from catalog newsitem

--- wcc/weeklynews/browser/weeklynews_view.py
+++ wcc/weeklynews/browser/weeklynews_view.py
@@ -1,6 +1,8 @@
 from five import grok
 from plone.directives import dexterity, form
 from wcc.weeklynews.content.weeklynews import IWeeklyNews
+from Products.CMFCore.utils import getToolByName
+from dateutil.parser import parse
 
 grok.templatedir('templates')
 
@@ -10,3 +12,13 @@ class Index(dexterity.DisplayForm):
     grok.template('weeklynews_view')
     grok.name('view')
 
+
+    def update(self):
+        self.catalog = getToolByName(self.context, 'portal_catalog')
+
+    @property
+    def allnews(self):
+        return self.catalog.searchResults(portal_type='News Item')
+
+    def convertdate(self, value):
+        return parse(value).strftime("%d, %B %Y")
