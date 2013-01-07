8088aebd6fee4d3ba89776d58c9f002c9a1c1361
kulll <anwarbaik88@gmail.com>
2013-01-07 23:24:04 +0800

    wcc.weeklynews:     * a not so good mock for weekltnews

--- wcc/weeklynews/browser/weeklynews_view.py
+++ wcc/weeklynews/browser/weeklynews_view.py
@@ -21,16 +21,38 @@ class Index(dexterity.DisplayForm):
         y = self.catalog.searchResults(portal_type='News Item')
         return [x for x in y if self.date_range(x.getObject())]
 
+    @property
+    def allevents(self):
+        y = self.catalog.searchResults(portal_type='Event')
+        end = self.context.endDate.date()
+        return [x for x in y if self.get_date(x.getObject()) > end]
+
+    @property
+    def allprayer(self):
+        y = self.catalog.searchResults(
+                portal_type='wcc.prayercycle.prayercycle')
+        end = self.context.endDate.date()
+        return [x for x in y if self.get_date(x.getObject())]
+
     def convert_date(self, value):
         return parse(value).strftime("%d %B %Y")
 
+    def get_date(self, data):
+        return parse(data.Date()).date()
+
     def date_title(self):
-        start = self.context.startDate.strftime("%d %b %y") 
-        end = self.context.endDate.strftime("%d %b %y") 
+        start = self.context.startDate.strftime("%d %b %y")
+        end = self.context.endDate.strftime("%d %b %y")
         return "<span> %s - %s </span>" % (start, end)
 
+    def prayerdate(self, prayerobject):
+        start = prayerobject.startDate.strftime("%d %b")
+        end = prayerobject.endDate.strftime("%d %b %y")
+        return "<span> %s - %s </span>" % (start, end)
+
+
     def date_range(self, data):
-        objectdate = parse(data.Date()).date()
+        objectdate = self.get_date(data)
         start = self.context.startDate.date()
         end = self.context.endDate.date()
         return start <= objectdate <= end
