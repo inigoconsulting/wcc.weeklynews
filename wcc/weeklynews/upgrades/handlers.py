7d050206057fd24935ddaa79005d3ce38cb3c0be
Izhar Firdaus <izhar@inigo-tech.com>
2013-01-31 11:47:40 +0800

    add upgrade profile (re #146)

--- wcc/weeklynews/upgrades/handlers.py
+++ wcc/weeklynews/upgrades/handlers.py
@@ -2,3 +2,12 @@ from collective.grok import gs
 from Products.CMFCore.utils import getToolByName
 
 # -*- extra stuff goes here -*- 
+
+
+@gs.upgradestep(title=u'Upgrade wcc.weeklynews to 1001',
+                description=u'Upgrade wcc.weeklynews to 1001',
+                source='1', destination='1001',
+                sortkey=1, profile='wcc.weeklynews:default')
+def to1001(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-wcc.weeklynews.upgrades:to1001')
